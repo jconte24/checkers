ea3bbf1a40e3a0a27d3b81b3f1dc74c7c51aff7a
justdan325 <35572086+justdan325@users.noreply.github.com>
2018-03-22 19:06:27 -0400

    Functionality Fix

--- FrontEndSimulator2.java
+++ FrontEndSimulator2.java
@@ -517,7 +517,7 @@ public class FrontEndSimulator2 extends JFrame {
             ip = jTextField3.getText();
             if(ip!=null && !ip.equals("") && !ip.equals(" "))
             {
-                control = new ControlUnit(ip);
+                control = new ControlUnit(ip, this);
 				
 				TimeUnit.MILLISECONDS.sleep(250);
 				
